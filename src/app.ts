b823636ef0b8ad35ac0bed5218c2e2d72572cfd6
Jonas Nilsson <nilssoncjonas@gmail.com>
2023-02-10 10:38:50 +0100

    added app.use cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import express from "express"
 import morgan from "morgan"
 import routes from './routes'
+import cors from "cors"
 
 const app = express()
 app.use(express.json())
 app.use(morgan('dev'))
-
+app.use(cors())
 // Use routes
 app.use(routes)
 
