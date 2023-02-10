393dd7bbf389214c8a79cf38f62d6dc01b4a402d
Jonas Nilsson <nilssoncjonas@gmail.com>
2023-02-10 12:58:22 +0100

    deployment error

--- prisma/migrations/20230210100619_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE `Quotes` (
-    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-    `content` TEXT NOT NULL,
-    `name` VARCHAR(191) NOT NULL,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
